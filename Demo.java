e231e11762e984ea6340621d92d73779cf01dfe4
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-09-09 08:58:55 +0530

    feat: create basic example Demo.java

---
+++ Demo.java
@@ -0,0 +1,11 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+class Demo {
+    public static void main(String[] args) {
+        if(args.length==0) {
+            System.out.println("Hello World!");
+        } else {
+            System.out.println("Hello " + args[0]);
+        }
+    }
+}
